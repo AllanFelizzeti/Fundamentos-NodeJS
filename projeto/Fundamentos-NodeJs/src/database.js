b5aaa9148567192bf1b0f0eb809c2f59b3906c18
Allan Felizzeti <alllanvergilio@gmail.com>
2023-01-16 14:41:07 -0300

    Rotas da aplicação

--- projeto/Fundamentos-NodeJs/src/database.js
+++ projeto/Fundamentos-NodeJs/src/database.js
@@ -7,7 +7,7 @@ export class Database {
 
   constructor() {
     fs.readFile(databasePath, 'utf8',).then(data => {
-      this.database = JSON.parse(data)
+      this.#database = JSON.parse(data)
     })
     .catch(() => {
       this.#persist()
@@ -18,8 +18,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].includes(value)
+        })
+      })
+    }
 
     return data
   }
@@ -31,8 +39,26 @@ export class Database {
       this.#database[table] = [data]
     }
 
-    this.#persist();
+    this.#persist()
 
-    return data;
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id == id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
+  dalete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id == id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
   }
 }
